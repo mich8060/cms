f356689b2bb2498217601473d084a71deafdf127
Michael Stevens <mich8060@hotmail.com>
2014-01-15 16:08:47 -0700

    First Commit
    Adding every file to our repository

---
+++ application/views/site.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+<title>CMS</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/reset.css" />
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" />
+<link rel="stylesheet" href="css/grids.css" />
+<link rel="stylesheet" href="css/screen.css" />
+</head>
+<body>
+<article>
+	<div class="container">
+		<div class="boxgrid">
+			<div class="grid_1">
+				&nbsp;
+				<nav>
+					<a href="#" class="profile"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/acoops_/128.jpg" /></a>
+					<a href="#">General</a>
+					<a href="#">Content</a>
+					<a href="#">Accounts</a>
+					<a href="#">Media</a>
+					<a href="#" class="selected">Settings</a>
+				</nav>
+			</div>
+			<div class="grid_3">
+				<menu>
+					<h3 class="light">Settings</h3>
+					<p class="paragraph-hint">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin cursus, elit vitae.
+					</p>
+					<ul>
+						<li><a href="#">General Info</a></li>
+						<li><a href="#">Users</a></li>
+						<li><a href="#" class="selected">Comments <span class="ui-notification">2</span></a></li>
+					</ul>
+				</menu>
+			</div>
+			<div class="grid_8">
+				<section id="content">
+					<div class="intro">
+						<h1 class="light">Comments</h1>
+						<ul class="ui-tabs">
+							<li><a href="#" class="selected">Recent <span class="ui-notification">2</span></a></li>
+							<li><a href="#">Approved</a></li>
+							<li><a href="#">Flagged</a></li>
+						</ul>
+						<a href="#" class="ui-button">Save Changes</a>
+					</div>
+					<hr />
+					<div class="content-container">
+						Content
+					</div>
+				</section>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+</article>
+</body>
+</html>
