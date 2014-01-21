ed93f706c86702de1621fbb30499ee6e79760111
Michael Stevens <mich8060@hotmail.com>
2014-01-21 13:18:35 -0700

    Reformatting for full height

--- application/views/site.php
+++ application/views/site.php
@@ -11,50 +11,46 @@
 <body>
 <article>
 	<div class="container">
-		<div class="boxgrid">
-			<div class="grid_1">
-				&nbsp;
-				<nav>
-					<a href="#" class="profile"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/acoops_/128.jpg" /></a>
-					<a href="#">General</a>
-					<a href="#">Content</a>
-					<a href="#">Accounts</a>
-					<a href="#">Media</a>
-					<a href="#" class="selected">Settings</a>
-				</nav>
-			</div>
-			<div class="grid_3">
-				<menu>
-					<h3 class="light">Settings</h3>
-					<p class="paragraph-hint">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin cursus, elit vitae.
-					</p>
-					<ul>
-						<li><a href="#">General Info</a></li>
-						<li><a href="#">Users</a></li>
-						<li><a href="#" class="selected">Comments <span class="ui-notification">2</span></a></li>
-					</ul>
-				</menu>
-			</div>
-			<div class="grid_8">
-				<section id="content">
-					<div class="intro">
-						<h1 class="light">Comments</h1>
+		<nav>
+			<a href="#" class="profile"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/acoops_/128.jpg" /></a>
+			<a href="#">Dashboard</a>
+			<a href="#">Content</a>
+			<a href="#">Media</a>
+			<a href="#" class="selected">Accounts</a>
+			<a href="#" class="logout">Logout</a>
+		</nav>
+		<menu>
+			<h3 class="light">Accounts</h3>
+			<p class="paragraph-hint">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin cursus, elit vitae.
+			</p>
+			<ul>
+				<li><a href="#" class="selected">Users</a></li>
+				<li><a href="#">Administrators</a></li>
+				<li><a href="#">Developers</a></li>
+				<li><a href="#">Mailing List</a></li>
+			</ul>
+		</menu>	
+		<section id="content">
+			<div class="boxgrid">
+				<div class="grid_12">
+					<div class="content-header">
+						<h3 class="light">Comments</h3>
 						<ul class="ui-tabs">
-							<li><a href="#" class="selected">Recent <span class="ui-notification">2</span></a></li>
+							<li><a href="#" class="selected">Pending<span class="ui-notification">2</span></a></li>
 							<li><a href="#">Approved</a></li>
 							<li><a href="#">Flagged</a></li>
 						</ul>
 						<a href="#" class="ui-button">Save Changes</a>
 					</div>
-					<hr />
 					<div class="content-container">
 						Content
 					</div>
-				</section>
+				</div>
 			</div>
-		</div>
-		<div class="clear"></div>
+			<div class="clear"></div>
+			
+		</section>
 	</div>
 </article>
 </body>
